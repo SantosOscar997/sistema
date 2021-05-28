f10718d36b20b755016e2497fd7d862cb83d3314
Oscar Santos Ortiz <santosortizoscar.itz@outlook.com>
2021-05-28 14:04:09 -0500

    Second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { EmpleadosModule } from './empleados/empleados.module';
+import { MenuComponent } from './components/menu/menu.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
+    UsuariosModule,
+    EmpleadosModule,
     AppRoutingModule
   ],
   providers: [],
